b7f2bb8904aeacffc905933be522512727680a8f
Diya <89987443+divyamishra7@users.noreply.github.com>
2022-08-20 01:45:18 +0530

    Time: 124 ms (77.57%), Space: 58.5 MB (41.56%) - LeetHub

---
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+       unordered_map<int, int> left, end;
+        for (int i: nums) {
+            left[i]++;
+        }
+        for (int i: nums) {
+            if (left[i] == 0) continue;
+            left[i]--;
+            if (end[i - 1] > 0) {   
+                end[i - 1]--;
+                end[i]++;
+            } else if (left[i + 1] > 0 && left[i + 2] > 0) {
+                left[i + 1]--;
+                left[i + 2]--;
+                end[i + 2]++;
+            } else {   
+                return false;
+            }
+        }
+        return true; 
+    }
+};
+//
+    // bool isPossible(vector<int>& A) {
+    //     unordered_map<int, int> left, end;
+    //     for (int i: A) {
+    //         left[i]++;
+    //     }
+    //     for (int i: A) {
+    //         if (left[i] == 0) continue;
+    //         left[i]--;
+    //         if (end[i - 1] > 0) {   
+    //             end[i - 1]--;
+    //             end[i]++;
+    //         } else if (left[i + 1] > 0 && left[i + 2] > 0) {
+    //             left[i + 1]--;
+    //             left[i + 2]--;
+    //             end[i + 2]++;
+    //         } else {   
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
