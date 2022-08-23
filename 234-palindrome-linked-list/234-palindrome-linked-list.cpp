02a323888f324576805c240ae3c0f732bc4043da
Diya <89987443+divyamishra7@users.noreply.github.com>
2022-08-23 13:47:06 +0530

    Time: 363 ms (53.45%), Space: 120.7 MB (40.79%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -11,39 +11,59 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        stack<int> s;
-        ListNode* fast = head;
         ListNode* slow = head;
-        if(head -> next == NULL)
-            return true;
-        
-        while(fast->next != NULL){
-            //s.push(slow->val);
-            //cout<<"fast : "<<fast->val<<" slow : "<<slow->val<<endl;
+        ListNode* fast = head;
+        stack<int> s;
+        bool moveSlow = true;
+        while(fast -> next != NULL){
+            s.push(slow->val);
+            //cout<<slow->val<<",";
             if(fast -> next -> next == NULL){
-                //slow = slow -> next;
-                fast = fast -> next;
+                //cout<<"camehere"<<endl;
+                moveSlow = false;
+                slow = slow -> next;
                 break;
             }
-                
+            
+            
+            slow = slow -> next;
             fast = fast -> next -> next;
-            slow = slow -> next;
         }
-        slow = slow -> next;
+        if(moveSlow == true)
+            slow = slow -> next;
+        //cout<<"after loop"<<slow->val;
         while(slow != NULL){
-            
-            s.push(slow-> val);
-            slow = slow -> next;
-            
-            
-        }
-        ListNode* iter = head;
-        while(!s.empty()){
-            if(s.top() != iter->val)
+            //cout<<slow->val<<":"<<s.top();
+            if(slow->val != s.top())
                 return false;
             s.pop();
-            iter = iter -> next;
+            slow= slow->next;
         }
         return true;
     }
 };
+
+
+//
+// class Solution {
+// public:
+//     bool isPalindrome(ListNode* head) {
+//         ListNode* fast = head, *slow = head;                // for finding the middle of the linked list 
+//         ListNode *prev = nullptr, *next;                // for reversing the first half of the linked list
+//         while(fast && fast->next) {
+//             fast = fast->next->next;
+//             next = slow->next;                          // reverse logic beginning
+//             slow->next = prev;
+//             prev = slow;
+//             slow = next;                                // reverse logic ending
+//         }
+//         slow = fast ? slow->next : slow;                // check if slow is actually at middle or not
+//         while(slow != nullptr) {
+//             if(slow->val != prev->val)                  // palindrome logic
+//                 return false;
+//             slow = slow->next;
+//             prev = prev->next;
+//         }
+//         return true;
+//     }
+// };
