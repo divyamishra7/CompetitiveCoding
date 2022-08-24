0bd85b93abc6c63c07dc6fcb300cb667a6647742
Diya <89987443+divyamishra7@users.noreply.github.com>
2022-08-25 03:41:15 +0530

    Time: 146 ms (77.25%), Space: 14.9 MB (21.60%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int i = matrix.size() - 1;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int i = m-1;
         int j = 0;
-        
-        return smHelper(matrix, target, i, j);
-        
-        
-    }
-    bool smHelper(vector<vector<int>>& matrix, int target, int i, int j){
-        //cout<<"i : "<<i<<" j : "<<j<<"\n";
-
-        if(i < 0 || j < 0 || i>= matrix.size() || j>= matrix[0].size()){
-            // cout<<"at false i : "<<i<<" j : "<<j<<"\n";
-            // cout<<"matrix size : "<<matrix.size();
-            return false;
+        while(i >= 0 && i < m && j >= 0 && j < n){
+            //cout<<matrix[i][j]<<endl;
+            if(matrix[i][j] == target) return true;
+            if(matrix[i][j] < target) {
+                j++;
+                continue;
+            }
+            if(matrix[i][j] > target) {
+                i--;
+                continue;
+            }
         }
-            
-        else if(matrix[i][j] == target)
-            return true;
-        else if(matrix[i][j] < target)
-            return smHelper(matrix, target, i,j+1);
-        else //if (matrix[i][j] > target)
-            return smHelper(matrix, target, i -1, j);
+        return false;
         
     }
-    
 };
