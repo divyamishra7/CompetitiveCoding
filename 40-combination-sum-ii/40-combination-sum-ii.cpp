950ad8993df7a84791df441469f3c8e7f0b12193
Diya <89987443+divyamishra7@users.noreply.github.com>
2022-08-25 02:35:59 +0530

    Time: 19 ms (23.06%), Space: 10.8 MB (47.47%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        set<vector<int>> result;
+        vector<int> temp;
+        sort(candidates.begin(), candidates.end());
+        helper(candidates, result, temp, 0, target);
+        vector<vector<int>> res(result.begin(), result.end());
+        return res;
+        
+    }
+    void helper(vector<int>& candidates, set<vector<int>> &result, vector<int> &temp, int index, int target){
+        //if(candidates[index] > target) return;
+        if(target < 0) return;
+        if(target == 0){
+            result.insert(temp);
+            return;
+        }
+        if(index >= candidates.size()) return;
+        
+        temp.push_back(candidates[index]);
+        helper(candidates, result, temp, index+1, target-candidates[index]);
+        int i = 1;
+        while(index+i<candidates.size() && candidates[index]==candidates[index+i]){
+         //if(index >= 0 && candidates[index] != candidates[index+1]) return;
+        i++;
+        }
+        temp.pop_back();
+        helper(candidates, result, temp, index+i, target);   
+            
+            
+        
+        
+            
+        
+    }
+};
